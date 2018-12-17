6a4a2b1ea0214d2d56c117b466877cce6c3cbe32
Jaquez Upton <btkhase@Jaquezs-MacBook-Pro.local>
2018-12-17 11:54:51 -0500

    12th commit addin click functions to points

--- Jeopardy.js
+++ Jeopardy.js
@@ -1,5 +1,45 @@
 $(".a1").on("click", function (){
     swal("write question here");
 })
-
-
+$(".a2").on("click", function (){
+    swal("write question here");
+})
+$(".a3").on("click", function (){
+    swal("write question here");
+})
+$(".a4").on("click", function (){
+    swal("write question here");
+})
+$(".a5").on("click", function (){
+    swal("write question here");
+})
+$(".c1").on("click", function (){
+    swal("write question here");
+})
+$(".c2").on("click", function (){
+    swal("write question here");
+})
+$(".c3").on("click", function (){
+    swal("write question here");
+})
+$(".c4").on("click", function (){
+    swal("write question here");
+})
+$(".c5").on("click", function (){
+    swal("write question here");
+})
+$(".g1").on("click", function (){
+    swal("write question here");
+})
+$(".g2").on("click", function (){
+    swal("write question here");
+})
+$(".g3").on("click", function (){
+    swal("write question here");
+})
+$(".g4").on("click", function (){
+    swal("write question here");
+})
+$(".g5").on("click", function (){
+    swal("write question here");
+})
