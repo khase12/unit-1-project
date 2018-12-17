2a4bba91d32c1559ab5410cbdc4b30582e5715e6
Jaquez Upton <btkhase@Jaquezs-MacBook-Pro.local>
2018-12-17 12:40:24 -0500

    14th commit done adding questions

--- Jeopardy.js
+++ Jeopardy.js
@@ -1,26 +1,27 @@
+// used sweet alert
 $(".a1").on("click", function (){
-    swal("write question here");
+    swal("This artist has the song name Gummo?");
 })
 $(".a2").on("click", function (){
-    swal("write question here");
+    swal("This artist told sway in the morning You Dont Have The Answers?");
 })
 $(".a3").on("click", function (){
-    swal("write question here");
+    swal("This artist droped his album called scorpion");
 })
 $(".a4").on("click", function (){
-    swal("write question here");
+    swal("This artist has a hit single called trip");
 })
 $(".a5").on("click", function (){
-    swal("write question here");
+    swal("This artist is known for his adlib Its Lit");
 })
 $(".c1").on("click", function (){
-    swal("write question here");
+    swal("When there's an error in your code this symbol is missing or misplaced?<");
 })
 $(".c2").on("click", function (){
-    swal("write question here");
+    swal("This symbol is used to your ID tag in the CSS?");
 })
 $(".c3").on("click", function (){
-    swal("write question here");
+    swal("Inside this file can alter thew apperance of the web page?");
 })
 $(".c4").on("click", function (){
     swal("write question here");
@@ -29,47 +30,47 @@ $(".c5").on("click", function (){
     swal("write question here");
 })
 $(".g1").on("click", function (){
-    swal("write question here");
+    swal("In this game you can explor and catch animals with powers");
 })
 $(".g2").on("click", function (){
-    swal("write question here");
+    swal("This game has a main character name goku");
 })
 $(".g3").on("click", function (){
-    swal("write question here");
+    swal("In this game you can either play multiplayer or zombies?");
 })
 $(".g4").on("click", function (){
-    swal("write question here");
+    swal("n this game you kill zombies working as a secret agent? ");
 })
 $(".g5").on("click", function (){
-    swal("write question here");
+    swal("In this game you play as two plumber brother?");
 })
 $(".m1").on("click", function (){
-    swal("write question here");
+    swal("n this movie kelvin hart was hired to be a best man?");
 })
 $(".m2").on("click", function (){
-    swal("write question here");
+    swal("This actor played in wolf of wall street and also inception?<");
 })
 $(".m3").on("click", function (){
-    swal("write question here");
+    swal("In this movie criag knocked debo out?");
 })
 $(".m4").on("click", function (){
-    swal("write question here");
+    swal(">In this movie thier native home was wakanda?");
 })
 $(".m5").on("click", function (){
-    swal("write question here");
+    swal("In this movie the hero real name is peter parker?");
 })
 $(".mo1").on("click", function (){
-    swal("write question here");
+    swal("This president is on the 1 dollar bill?");
 })
 $(".mo2").on("click", function (){
-    swal("write question here");
+    swal("This president is on the 5 dollar bill?");
 })
 $(".mo3").on("click", function (){
-    swal("write question here");
+    swal(">This president is on the 10 dollar bill?");
 })
 $(".mo4").on("click", function (){
-    swal("write question here");
+    swal("This president is on the 20 dollar bill?");
 })
 $(".mo5").on("click", function (){
-    swal("write question here");
+    swal("This president is on the 50 dollar bill?");
 })
