cdebea99955d4bf918c26a2192ef07b58130828d
Jaquez Upton <btkhase@Jaquezs-MacBook-Pro.local>
2018-12-17 22:03:02 -0500

    22nd commit adding answers

--- Jeopardy.js
+++ Jeopardy.js
@@ -3,10 +3,10 @@ $(".a1").on("click", function (){
     swal({
         text:"This artist has a song named Gummo?",
         buttons:{
-        A:
+        A:"Lil baby"
         B:
-        C:
-        B:
+        C:"Tekashi 69"
+        D: "Chief Keef"
         }
 });
 })
@@ -18,20 +18,20 @@ $(".a2").on("click", function(){
         swal({
         text: "This artist told sway in the morning You Dont Have The Answers?",
         buttons: {
-            a: "Kanye West",
-            b: "Drake", 
-            c: "Lil Wayne",
-            d: "G Herbo"
+            A: "Kanye West",
+            B: "Drake", 
+            C: "Lil Wayne",
+            D: "G Herbo"
         }}
         )})
 $(".a3").on("click", function (){
     swal({
         text:"This artist droped his album called scorpion",
         buttons: {
-            a: "Kanye West",
-            b: "Drake", 
-            c: "Lil Wayne",
-            d: "ffjfjf"
+            A: "Kanye West",
+            B: "Drake", 
+            C: "Lil Wayne",
+            D: "ffjfjf"
         }}
         )})
 $(".a4").on("click", function (){
@@ -41,7 +41,7 @@ $(".a4").on("click", function (){
         A:
         B:
         C:
-        B:
+        D:
         }
 })
 $(".a5").on("click", function (){
@@ -51,67 +51,207 @@ $(".a5").on("click", function (){
             A:
             B:
             C:
-            B:
+            D:
             }
 })
 $(".c1").on("click", function (){
     swal({
-        text:"When there's an error in your code this symbol is missing or misplaced?");
+        text:"When there's an error in your code this symbol is missing or misplaced?",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".c2").on("click", function (){
-    swal("This symbol is used to your ID tag in the CSS?");
+    swal({
+        text:"This symbol is used to your ID tag in the CSS?",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".c3").on("click", function (){
-    swal("Inside this file can alter the apperance of the web page?");
+    swal({
+        text:"Inside this file can alter the apperance of the web page?",
+        
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".c4").on("click", function (){
-    swal("");
+    swal({
+        text:"",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".c5").on("click", function (){
-    swal("");
+    swal({
+        text:"",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".g1").on("click", function (){
-    swal("In this game you can explor and catch animals with powers");
+    swal({
+        text:"In this game you can explor and catch animals with powers",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".g2").on("click", function (){
-    swal("This game has a main character name goku");
+    swal({
+        text:"This game has a main character name goku",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".g3").on("click", function (){
-    swal("In this game you can either play multiplayer or zombies?");
+    swal({
+        text:"In this game you can either play multiplayer or zombies?",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".g4").on("click", function (){
-    swal("n this game you kill zombies working as a secret agent? ");
+    swal({
+        text:"In this game you kill zombies working as a secret agent?",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".g5").on("click", function (){
-    swal("In this game you play as two plumber brother?");
+    swal({
+        text:"In this game you play as two plumber brother?",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".m1").on("click", function (){
-    swal("n this movie kelvin hart was hired to be a best man?");
+    swal({
+        text:"n this movie kelvin hart was hired to be a best man?",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".m2").on("click", function (){
-    swal("This actor played in wolf of wall street and also inception?<");
+    swal({
+        text:"This actor played in wolf of wall street and also inception?<",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".m3").on("click", function (){
-    swal("In this movie criag knocked debo out?");
+    swal({
+        text:"In this movie criag knocked debo out?",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".m4").on("click", function (){
-    swal(">In this movie thier native home was wakanda?");
+    swal({
+        text:"In this movie thier native home was wakanda?",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".m5").on("click", function (){
-    swal("In this movie the hero real name is peter parker?");
+    swal({
+        text:"In this movie the hero real name is peter parker?",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".mo1").on("click", function (){
-    swal("This president is on the 1 dollar bill?");
+    swal({
+        text:"This president is on the 1 dollar bill?",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".mo2").on("click", function (){
-    swal("This president is on the 5 dollar bill?");
+    swal({
+        text:"This president is on the 5 dollar bill?",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".mo3").on("click", function (){
-    swal(">This president is on the 10 dollar bill?");
+    swal({
+        text:"This president is on the 10 dollar bill?",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".mo4").on("click", function (){
-    swal("This president is on the 20 dollar bill?");
+    swal({
+        text:"This president is on the 20 dollar bill?",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
 })
 $(".mo5").on("click", function (){
-    swal("This president is on the 50 dollar bill?");
-})
+    swal({
+        text:"This president is on the 50 dollar bill?",
+        buttons:{
+            A:
+            B:
+            C:
+            D:
+            }
+    
