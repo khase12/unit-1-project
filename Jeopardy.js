0ea0c588fbdbe381d686769a8fec2e4912e5e301
Jaquez Upton <btkhase@Jaquezs-MacBook-Pro.local>
2018-12-17 22:41:30 -0500

    23rd commit still appling answers

--- Jeopardy.js
+++ Jeopardy.js
@@ -3,9 +3,9 @@ $(".a1").on("click", function (){
     swal({
         text:"This artist has a song named Gummo?",
         buttons:{
-        A:"Lil baby"
-        B:
-        C:"Tekashi 69"
+        A:"Lil baby",
+        B:"Gunna",
+        C:"Tekashi 69",
         D: "Chief Keef"
         }
 });
@@ -19,59 +19,59 @@ $(".a2").on("click", function(){
         text: "This artist told sway in the morning You Dont Have The Answers?",
         buttons: {
             A: "Kanye West",
-            B: "Drake", 
-            C: "Lil Wayne",
-            D: "G Herbo"
+            B: "Wale", 
+            C: "Drake",
+            D: "Rick Ross"
         }}
         )})
 $(".a3").on("click", function (){
     swal({
         text:"This artist droped his album called scorpion",
         buttons: {
-            A: "Kanye West",
+            A: "G Herbo",
             B: "Drake", 
-            C: "Lil Wayne",
-            D: "ffjfjf"
+            C: "Kanye West",
+            D: "Gucci Mane"
         }}
         )})
 $(".a4").on("click", function (){
     swal({
     text:"This artist has a hit single called trip",
     buttons:{
-        A:
-        B:
-        C:
-        D:
+        A:"Ella Mai",
+        B:"Cardi B ",
+        C:"Nicki Manaj",
+        D:"Remy Ma"
         }
 })
 $(".a5").on("click", function (){
     swal({
         text:"This artist is known for his adlib Its Lit",
         buttons:{
-            A:
-            B:
-            C:
-            D:
+            A:"Young Thug",
+            B:" Lil Uzi Vert",
+            C:"Travis Scott",
+            D:"Trippie Redd"
             }
 })
 $(".c1").on("click", function (){
     swal({
         text:"When there's an error in your code this symbol is missing or misplaced?",
         buttons:{
-            A:
-            B:
-            C:
-            D:
+            A:"{}",
+            B:".",
+            C:"()",
+            D:";"
             }
 })
 $(".c2").on("click", function (){
     swal({
         text:"This symbol is used to your ID tag in the CSS?",
         buttons:{
-            A:
-            B:
-            C:
-            D:
+            A:".",
+            B:":",
+            C:"#",
+            D:","
             }
 })
 $(".c3").on("click", function (){
@@ -79,19 +79,19 @@ $(".c3").on("click", function (){
         text:"Inside this file can alter the apperance of the web page?",
         
         buttons:{
-            A:
-            B:
-            C:
-            D:
+            A:"Html",
+            B:"CSS",
+            C:"javascript",
+            D:"jquery"
             }
 })
 $(".c4").on("click", function (){
     swal({
         text:"",
         buttons:{
-            A:
-            B:
-            C:
+            A:"",
+            B:"",
+            C:"",
             D:
             }
 })
@@ -99,9 +99,9 @@ $(".c5").on("click", function (){
     swal({
         text:"",
         buttons:{
-            A:
-            B:
-            C:
+            A:"",
+            B:"",
+            C:"",
             D:
             }
 })
@@ -109,69 +109,69 @@ $(".g1").on("click", function (){
     swal({
         text:"In this game you can explor and catch animals with powers",
         buttons:{
-            A:
-            B:
-            C:
-            D:
+            A:"Spider Man",
+            B:"Pokemon",
+            C:"Super Mario",
+            D:"Tomb Raider"
             }
 })
 $(".g2").on("click", function (){
     swal({
         text:"This game has a main character name goku",
         buttons:{
-            A:
-            B:
-            C:
-            D:
+            A:"Drangon Ball Z",
+            B:"Steet Fighters",
+            C:"Mortal Kombat",
+            D:"Fight Night"
             }
 })
 $(".g3").on("click", function (){
     swal({
         text:"In this game you can either play multiplayer or zombies?",
         buttons:{
-            A:
-            B:
-            C:
-            D:
+            A:"Fortnite",
+            B:"Battle Field",
+            C:"Call Of Duty",
+            D:"Batman"
             }
 })
 $(".g4").on("click", function (){
     swal({
         text:"In this game you kill zombies working as a secret agent?",
         buttons:{
-            A:
-            B:
-            C:
-            D:
+            A:"Dead Island",
+            B:"Resident Evil",
+            C:"Dead Or Alive",
+            D:"Dying Light"
             }
 })
 $(".g5").on("click", function (){
     swal({
         text:"In this game you play as two plumber brother?",
         buttons:{
-            A:
-            B:
-            C:
-            D:
+            A:"Super Smash Bros",
+            B:"God Of War",
+            C:"For Honored",
+            D:"Super Mario"
             }
 })
 $(".m1").on("click", function (){
     swal({
         text:"n this movie kelvin hart was hired to be a best man?",
         buttons:{
-            A:
-            B:
-            C:
-            D:
+            A:"Seriously Funny",
+            B:"Ride Along",
+            C:"Night School",
+            D:""
             }
 })
 $(".m2").on("click", function (){
     swal({
         text:"This actor played in wolf of wall street and also inception?<",
         buttons:{
-            A:
-            B:
-            C:
+            A:"",
+            B:"",
+            C:"",
             D:
             }
 })
@@ -179,9 +179,9 @@ $(".m3").on("click", function (){
     swal({
         text:"In this movie criag knocked debo out?",
         buttons:{
-            A:
-            B:
-            C:
+            A:"",
+            B:"",
+            C:"",
             D:
             }
 })
@@ -189,9 +189,9 @@ $(".m4").on("click", function (){
     swal({
         text:"In this movie thier native home was wakanda?",
         buttons:{
-            A:
-            B:
-            C:
+            A:"",
+            B:"",
+            C:"",
             D:
             }
 })
@@ -199,9 +199,9 @@ $(".m5").on("click", function (){
     swal({
         text:"In this movie the hero real name is peter parker?",
         buttons:{
-            A:
-            B:
-            C:
+            A:"",
+            B:"",
+            C:"",
             D:
             }
 })
@@ -209,49 +209,49 @@ $(".mo1").on("click", function (){
     swal({
         text:"This president is on the 1 dollar bill?",
         buttons:{
-            A:
-            B:
-            C:
-            D:
+            A:"Abraham Lincoln",
+            B:"Benjamin Franklin",
+            C:"Andrew Jackson",
+            D:"George Washington"
             }
 })
 $(".mo2").on("click", function (){
     swal({
         text:"This president is on the 5 dollar bill?",
         buttons:{
-            A:
-            B:
-            C:
-            D:
+            A:"",
+            B:"Abraham Lincoln",
+            C:"Benjamin Franklin",
+            D:"William McKinley."
             }
 })
 $(".mo3").on("click", function (){
     swal({
         text:"This president is on the 10 dollar bill?",
         buttons:{
-            A:
-            B:
-            C:
-            D:
+            A:"William McKinley.",
+            B:"",
+            C:"Alexander Hamilton",
+            D:""
             }
 })
 $(".mo4").on("click", function (){
     swal({
         text:"This president is on the 20 dollar bill?",
         buttons:{
-            A:
-            B:
-            C:
-            D:
+            A:"",
+            B:"Andrew Jackson",
+            C:"",
+            D:""
             }
 })
 $(".mo5").on("click", function (){
     swal({
         text:"This president is on the 50 dollar bill?",
         buttons:{
-            A:
-            B:
-            C:
-            D:
+            A:"",
+            B:"",
+            C:"",
+            D:"Ulysses S. Grant"
             }
     
